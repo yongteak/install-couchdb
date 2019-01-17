2655281e8d1ecee1239d36e931c0845af544e555
kown <skillove@gmail.com>
2019-01-17 10:37:35 +0900

    Update install-couchdb.sh

--- install-couchdb.sh
+++ install-couchdb.sh
@@ -12,10 +12,10 @@ sudo apt-get --no-install-recommends -y install \
     build-essential pkg-config runit curl \
     libicu-dev libmozjs185-dev libcurl4-openssl-dev
     
-wget http://apache-mirror.rbc.ru/pub/apache/couchdb/source/2.3.0/apache-couchdb-2.3.0.tar.gz
+wget https://github.com/apache/couchdb/releases/download/2.2.0/apache-couchdb-2.2.0.tar.gz
 
-tar -xvzf apache-couchdb-2.3.0.tar.gz
-cd apache-couchdb-2.3.0/
+tar -xvzf apache-couchdb-2.2.0.tar.gz
+cd apache-couchdb-2.2.0/
 ./configure && make release
 
 sudo adduser --system \
